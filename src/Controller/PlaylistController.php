8b8104fb1e042f1a8444caf39d6437bdc437ed64
Volodymyr Gulchuk <volodymyr@gulchuk.com>
2019-12-29 18:30:49 +0200

    Playlist generated CRUD

---
+++ src/Controller/PlaylistController.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Playlist;
+use App\Form\PlaylistType;
+use App\Repository\PlaylistRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/playlist")
+ */
+class PlaylistController extends AbstractController
+{
+    /**
+     * @Route("/", name="route_playlist_index", methods={"GET"})
+     */
+    public function index(PlaylistRepository $playlistRepository): Response
+    {
+        return $this->render('playlist/index.html.twig', [
+            'playlists' => $playlistRepository->findAll(),
+        ]);
+    }
+
+    /**
+     * @Route("/new", name="route_playlist_new", methods={"GET","POST"})
+     */
+    public function new(Request $request): Response
+    {
+        $playlist = new Playlist();
+        $form = $this->createForm(PlaylistType::class, $playlist);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($playlist);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('playlist_index');
+        }
+
+        return $this->render('playlist/new.html.twig', [
+            'playlist' => $playlist,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="route_playlist_show", methods={"GET"})
+     */
+    public function show(Playlist $playlist): Response
+    {
+        return $this->render('playlist/show.html.twig', [
+            'playlist' => $playlist,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}/edit", name="route_playlist_edit", methods={"GET","POST"})
+     */
+    public function edit(Request $request, Playlist $playlist): Response
+    {
+        $form = $this->createForm(PlaylistType::class, $playlist);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('route_playlist_index');
+        }
+
+        return $this->render('playlist/edit.html.twig', [
+            'playlist' => $playlist,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="route_playlist_delete", methods={"DELETE"})
+     */
+    public function delete(Request $request, Playlist $playlist): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$playlist->getId(), $request->request->get('_token'))) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->remove($playlist);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('route_playlist_index');
+    }
+}
